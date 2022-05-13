ed517b01e5a4634bb95d4ac518d0cdaca023a9e2
myuksal <ffgghhvvii4@gmail.com>
2022-05-13 14:50:00 +0900

    feat: pipe

--- functions/__init__.py
+++ functions/__init__.py
@@ -4,5 +4,4 @@ from functions.is_truthy import *
 from functions.any import *
 from functions.reduce import *
 from functions.adjust import *
-# TODO - pipe function
-# from functions.pipe import *
+from functions.pipe import *
---
+++ test/test_pipe.py
@@ -0,0 +1,15 @@
+from functions import pipe
+
+
+def _inc(x:int) -> int:
+  return x + 1
+
+
+def test_answer():
+  assert pipe(
+    _inc,
+    _inc,
+    _inc,
+    _inc,
+    _inc,
+  )(1) == 6
