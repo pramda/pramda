738f58ac6d64568bee1894d98124288afced1450
dbgus <dbgus0101@gmail.com>
2022-05-16 12:42:47 +0900

    feat: code style

--- functions/gt_test.py
+++ functions/gt_test.py
@@ -2,7 +2,7 @@ from .gt import gt
 
 
 def test_answer():
-    assert (gt(1, 2)) == False
-    assert (gt(2, 2)) == False
-    assert (gt(2.1, 2)) == True
-    assert (gt(3, 2)) == True
+    assert not gt(1, 2)
+    assert not gt(2, 2)
+    assert gt(2.1, 2)
+    assert gt(3, 2)
