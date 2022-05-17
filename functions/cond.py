5fd772256ee0db5df37824fa86068d57fdea08f9
Zasz123 <31340140+Zasz123@users.noreply.github.com>
2022-05-17 13:32:25 +0900

    feat: cond (#20)
    * feat: Add cond test
    
    * feat: Add cond
    
    * fix: Update test
    
    * fix: Remove curry
    
    * fix: Update loop logic to recursive function
    
    * fix: Fix argument on test

---
+++ functions/cond.py
@@ -0,0 +1,15 @@
+from typing import Callable
+
+from .is_truthy import is_truthy
+
+
+def cond(*pairs: list[Callable, Callable]) -> Callable:
+    def inner(*args):
+        def loop(left_pairs: list[list[Callable, Callable]]):
+            if(len(left_pairs) == 0):
+                return None
+            if is_truthy(left_pairs[0][0](*args)):
+                return left_pairs[0][1](*args)
+            return loop(left_pairs[1:])
+        return loop(pairs)
+    return inner
---
+++ functions/cond_test.py
@@ -0,0 +1,30 @@
+from .curry import curry
+from .cond import cond
+
+@curry
+def _equal(a, b):
+    return a == b
+
+def _T(*_):
+    return True
+
+def _always(a):
+    def inner(*_):
+        return a
+    return inner
+
+def test_asnwer():
+    a = cond(
+        [_equal(1), _always("a")],
+        [_equal(100), _always("b")],
+        [_T, _always("c")]
+    )
+    assert a(1) == "a"
+    assert a(100) == "b"
+    assert a(1000) == "c"
+
+    b = cond(
+        [_equal(1), _always("b")]
+    )
+    assert b(1) == "b"
+    assert b(10) == None
