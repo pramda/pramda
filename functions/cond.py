b954db9f35922392d6da040dc33a89b016c426a5
Zasz123 <ygm12345@naver.com>
2022-05-17 13:26:19 +0900

    fix: Fix argument on test

--- functions/cond.py
+++ functions/cond.py
@@ -11,5 +11,5 @@ def cond(*pairs: list[Callable, Callable]) -> Callable:
             if is_truthy(left_pairs[0][0](*args)):
                 return left_pairs[0][1](*args)
             return loop(left_pairs[1:])
-        return loop(pairs[0])
+        return loop(pairs)
     return inner
--- functions/cond_test.py
+++ functions/cond_test.py
@@ -14,17 +14,17 @@ def _always(a):
     return inner
 
 def test_asnwer():
-    a = cond([
+    a = cond(
         [_equal(1), _always("a")],
         [_equal(100), _always("b")],
         [_T, _always("c")]
-    ])
+    )
     assert a(1) == "a"
     assert a(100) == "b"
     assert a(1000) == "c"
 
-    b = cond([
+    b = cond(
         [_equal(1), _always("b")]
-    ])
+    )
     assert b(1) == "b"
     assert b(10) == None
