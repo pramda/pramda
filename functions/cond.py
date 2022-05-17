694ae0768bc84bd2357da884e0fc0cfd4b5adde2
Zasz123 <ygm12345@naver.com>
2022-05-17 13:20:47 +0900

    fix: Update loop logic to recursive function

--- functions/cond.py
+++ functions/cond.py
@@ -1,15 +1,15 @@
-from typing import Any, Callable
+from typing import Callable
 
 from .is_truthy import is_truthy
 
 
-def cond(pairs: list[tuple[Callable[[Any], bool], Callable[[Any], Any]]]) -> Callable:
-    def inner(*args, **kwargs):
-        for p in pairs:
-            if len(p) != 2:
-                continue
-
-            if is_truthy(p[0](*args, **kwargs) if p[0].__code__.co_argcount > 0 else p[0]()):
-                return p[1]()
-        return None
+def cond(*pairs: list[Callable, Callable]) -> Callable:
+    def inner(*args):
+        def loop(left_pairs: list[list[Callable, Callable]]):
+            if(len(left_pairs) == 0):
+                return None
+            if is_truthy(left_pairs[0][0](*args)):
+                return left_pairs[0][1](*args)
+            return loop(left_pairs[1:])
+        return loop(pairs[0])
     return inner
