3ad27ed6063674464cae01cf601c16c7ca5a469f
Zasz123 <ygm12345@naver.com>
2022-05-20 01:07:26 +0900

    feat: Add filter test

---
+++ pramda/filter_test.py
@@ -0,0 +1,11 @@
+from pramda.gt import gt
+from .curry import curry
+from .filter import filter
+
+
+def _is_even(n: int):
+    return n % 2 == 0
+
+def test_answer():
+    assert filter(_is_even, [1, 2, 3, 4]) == [2, 4]
+    assert filter(gt(1), [3, 50, 99]) == []
