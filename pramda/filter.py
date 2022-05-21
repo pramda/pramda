abc24fedaeb33d13d403ab07222b3730bff0a91d
Zasz123 <ygm12345@naver.com>
2022-05-21 10:22:37 +0900

    style: Cleanup by pre commit

--- pramda/filter.py
+++ pramda/filter.py
@@ -4,11 +4,16 @@ from .curry import curry
 
 AT = TypeVar("AT")
 
+
 @curry
 def filter(func: Callable[[AT], bool], arr: list[AT]) -> list[AT]:
     def loop(before_items, item, left_items):
-        if(len(left_items) == 0):
+        if len(left_items) == 0:
             return before_items + item
-        return loop(before_items + item, [left_items[0]] if func(left_items[0]) else [], left_items[1:])
+        return loop(
+            before_items + item,
+            [left_items[0]] if func(left_items[0]) else [],
+            left_items[1:],
+        )
 
     return loop([], [arr[0]] if func(arr[0]) else [], arr[1:])
