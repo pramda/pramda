629e22b36882b941c31704c632b8dd646fdba416
myuksal <33453423+myuksal@users.noreply.github.com>
2022-06-13 07:51:20 +0900

    feat: update v1.1.1 (#33)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="pramda",
-    version="1.0.0",
+    version="1.1.1",
     description="Functional programming for python",
     long_description=long_description,
     long_description_content_type="text/markdown",
